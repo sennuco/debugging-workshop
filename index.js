f93bfec801829a41c6589d1e1bf1019770cddb63
Theresa <cyrustheresa@gmail.com>
2019-11-25 13:30:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  const username = document.getElementById('name-input')
   let joke;
-
+  
+  
+  
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
@@ -12,16 +13,59 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke
+      const newJokeLi = document.createElement('li')
+      const deleteBtn = deleteAJoke()
+
+      newJokeLi.innerHTML = `
+      <span class="username">${username.value} says:</span> ${joke} "🤣"
+      `
+
+      newJokeLi.append(deleteBtn)
+      jokeList.appendChild(newJokeLi)
+      
+      console.log(deleteBtn)
+    })
   }
 
+  
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-    if(username === "") return;
+    /*this states if the username.value is equal to an empty string
+    (meaning user did not enter anything for their name, then return empty)
+    I added .value so that the value of the input is not equal to empty, which allows
+    me to bypass the return
+    */
+    if(username.value === "") return;
+
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
   })
+
+//create delete button
+
+
+
+ 
+
+
+
+  function deleteAJoke(){
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerHTML= "Delete"
+    deleteBtn.setAttribute('class','delete-btn')
+
+    deleteBtn.addEventListener("click",(evt) => {
+     let theParentOfDeleteButton = evt.target.parentElement
+      theParentOfDeleteButton.remove()
+    })
+    return deleteBtn //return delete , must return or the delete button will be undefined
+  }
+
+
+  
+
+
+
 })
